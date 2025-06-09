ce4bd15737b44c0f6b4d7ade05ac8d71b9fc1b81
AlexCrs-hub <achi.grigore@gmail.com>
2025-06-09 17:22:38 +0300

    forgot verification of token when getting readings

--- routes/reading.routes.js
+++ routes/reading.routes.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const readingController = require('../controllers/reading.controller');
+const isAuth = require('../middlewares/verifyToken');
 
 const router = express.Router();
 
 // Controller functions (replace with your actual controller)
 
 // Get all readings
-router.get('/', readingController.getAllReadings);
+router.get('/', isAuth.verifyToken, readingController.getAllReadings);
 
 // Create a new reading
 router.post('/', readingController.createReading);
