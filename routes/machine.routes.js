66a3880b4627e501f500a11889dbd2534ad6a95a
AlexCrs-hub <achi.grigore@gmail.com>
2026-03-11 21:52:00 +0200

    api key modifications

--- routes/machine.routes.js
+++ routes/machine.routes.js
@@ -12,7 +12,7 @@ router.get('/report', isAuth.verifyToken, machineController.getMachineReport);
 router.get('/name/:name', serviceAuth.verifyServiceKey, machineController.getMachineByName);
 router.get('/:id',isAuth.verifyToken, machineController.getMachineById);
 router.put('/:id',isAuth.verifyToken, machineController.updateMachineById);
-router.delete('/:id',isAuth.verifyToken, machineController.deleteMachine);
-router.get('/',isAuth.verifyToken, machineController.getUserMachines);
+router.delete('/:id', isAuth.verifyToken, machineController.deleteMachine);
+router.get('/', isAuth.verifyToken, machineController.getUserMachines);
 
 module.exports = router;
